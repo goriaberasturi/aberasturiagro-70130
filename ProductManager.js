3b3a84943d04f350611369661ac28ace52a89daa
gregorio aberasturi <gregorioaberasturi@gmail.com>
2024-07-19 02:08:15 -0300

    Optimizado, testeado y listo para el segundo desafio entregable

--- ProductManager.js
+++ ProductManager.js
@@ -1,4 +1,6 @@
+const { error } = require('console');
 const fs = require('fs');
+const { title } = require('process');
 
 class ProductManager {
     #id = 0;
@@ -31,33 +33,27 @@ class ProductManager {
             const product = products.find(prod => prod.id == id);
 
             if (product) {
-                return product;
+                return [product, products];
             } else {
-                return undefined;
+                throw new Error('Product not found');
             }
 
         } catch (error) {
-            return [];
+            return console.log(`ID ${error}`);
         }
     }
 
-    async codeValidated(code) {
-        try {
-            const products = await this.getProducts();
-            const product = products.find(product => product.code == code);
+    async isCodeRegistered(code, products) {
+        const product = products.find(product => product.code == code);
 
-            if (product) {
-                return false;
-            } else {
-                return true;
-            }
-
-        } catch (error) {
-            return error;
+        if (product) {
+            throw new Error('This code is already registered');
+        } else {
+            return false;
         }
     }
 
-    async fieldValidated(product) {
+    async isfieldValid(product) {
         let isValid = true;
         let fields = Object.values(product);
 
@@ -67,56 +63,86 @@ class ProductManager {
             }
         });
 
-        return isValid;
+        if (isValid) {
+            return true;
+        } else {
+            throw new Error('There is an empty field');
+        }
     }
 
     async addProducts(prod) {
-        await this.initializeId();
-        
-        const product = {
-            title: prod.title,
-            description: prod.description,
-            price: prod.price,
-            thumbnail: prod.thumbnail,
-            code: prod.code,
-            stock: prod.stock,
-            id: this.#id + 1,
+        try {
+
+            await this.initializeId();
+
+            const products = await this.getProducts();
+
+            const product = {
+                title: prod.title,
+                description: prod.description,
+                price: prod.price,
+                thumbnail: prod.thumbnail,
+                code: prod.code,
+                stock: prod.stock,
+                id: this.#id + 1,
+            }
+
+            await this.isfieldValid(product);
+
+            await this.isCodeRegistered(product.code, products);
+
+            products.push(product);
+
+            this.#id++;
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
+        } catch (error) {
+            console.log(error);
         }
-
-        const fieldValid = await this.fieldValidated(product);
-        const codeValid = await this.codeValidated(product.code);
-
-        if (!fieldValid) return console.log('There is an empty field');
-
-        if (!codeValid) return console.log('Theres is already a product with this code');
-
-        const products = await this.getProducts();
-        products.push(product);
-        this.#id++;
-        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
     }
 
     async updateProduct(id, fields) {
-        const products = await this.getProducts();
-        let product = await this.getProductById(id);
+        try {
+            const [product, products] = await this.getProductById(id);
 
-        if (product) {
-            const fieldValid = await this.fieldValidated(fields);
+            if (product) {
+                await this.isfieldValid(fields);
 
-            if (!fieldValid) return console.log('There is an empty field');
+                if (Object.keys(fields).includes('code')) {
+                    await this.isCodeRegistered(fields.code, products);
+                }
 
-            if (Object.keys(fields).includes('code')) {
-                if (await this.codeValidated(fields.code)) return 'This code is already registered';
+                const updatedProduct = { ...product, ...fields, id };
+
+                const index = products.findIndex(prod => prod.id === updatedProduct.id);
+                products[index] = updatedProduct;
+
+                await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
+            } else {
+                throw new Error('Product not found');
             }
 
-            const updatedProduct = { ...product, ...fields, id };
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-            const index = products.findIndex(prod => prod.id === updatedProduct.id);
-            products[index] = updatedProduct;
+    async deleteProduct(id) {
+        try {
+            const [product, products] = await this.getProductById(id);
+    
+            if (product) {
+                const index = products.findIndex(prod => prod.id === id);
+                products.splice(index, 1);
+    
+                await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
+    
+            } else {
+                return console.log('Product not found');
+            }
 
-            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2), 'utf-8');
-        } else {
-            return console.log('Product not found');
+        } catch(error) {
+            console.log(error);
         }
     }
 }
@@ -125,27 +151,32 @@ const productManager = new ProductManager('./products.json');
 
 const prodPrueba = {
     title: 'producto prueba',
-    description: 'Este es un producto de prueba',
+    description: 'Este es un producto prueba',
     price: 200,
     thumbnail: 'Sin imagen',
-    code: 'ab234',
+    code: 'abcd1234',
     stock: 25
 }
 
 const campos = {
-    price: 3000,
-    stock: 2,
+    title: 'Impresora Epson FX-390 II',
+    description: 'Impresora de matriz de puntos Epson FX-390 II',
+    price: 470000,
+    code: 'abcd1234'
 }
 
 const test = async () => {
-    // await productManager.addProducts(prodPrueba);
+    // console.log(await productManager.getProducts());
 
-    // let prod = await productManager.getProductById(1);
+    // await productManager.addProducts(prodPrueba);
+    // console.log(await productManager.getProducts());
+
+    // const [prod] = await productManager.getProductById(0);
     // console.log(prod);
 
-    await productManager.updateProduct(4, campos);
-    listado = await productManager.getProducts();
+    // await productManager.updateProduct(1, campos);
 
+    await productManager.deleteProduct(2);
 }
 
 test();
