0a82a08786f092e92ce704eff29457f74428ca74
gregorio aberasturi <gregorioaberasturi@gmail.com>
2024-07-19 23:11:48 -0300

    Agregado el query limit

--- app.js
+++ app.js
@@ -9,15 +9,23 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 app.get('/products', async (req, res) => {
+    const limit = req.query.limit;
     const products = await productManager.getProducts();
-    res.send(products);
+
+    if(!limit) return res.send(products);
+    if(isNaN(limit)) return res.send(products);
+
+    const productsFlt = products.splice(0, limit);
+
+    res.send(productsFlt);
 });
 
 app.get('/products/:pid', async (req, res) => {
     const productId = req.params.pid;
     const products = await productManager.getProducts();
+    
     if(isNaN(productId)) return res.send(products);
-
+    
     const [product] = await productManager.getProductById(productId);
     res.send(product);
 });
